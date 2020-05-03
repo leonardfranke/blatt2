00b75161679b359afeba46085b7bf3f5bc1e34d4
LeonardPC <leonard.franke@t-online.de>
2020-05-03 12:53:28 +0200

    TestClass merged

--- TestClass.java
+++ TestClass.java
@@ -1,24 +1,34 @@
 package blatt2;
 
 
-public class TestStack
+public class TestClass
 {
 	
 	private int errors = 0;
 
 	public static void main(String[] args)
 	{
-		TestStack test = new TestStack();
+		TestClass test = new TestClass();
+		System.out.println("Test started");
 		
 		test.testStackCopy();
 		test.testArea();
+		test.testDestruction();
 		
 		System.out.println(test.errors + " Fehler");
+		System.out.println("Test finished");
 
 	}
 	
 	private void assertString(String str1, String str2, String errorMsg) {
 		if(str1 == str2) {
+			System.out.println(errorMsg);
+			this.errors++;
+		}
+	}
+	
+	private void assertInt(int int1, int int2, String errorMsg) {
+		if(int1 != int2) {
 			System.out.println(errorMsg);
 			this.errors++;
 		}
@@ -33,15 +43,7 @@ public class TestStack
 		
 		assertString(stack1.pop(), stack2.pop(), "Error in testStackCopy of first element");		
 		assertString(stack1.pop(), stack2.pop(), "Error in testStackCopy of second element");		
-	}
-	
-	private void assertInt(int int1, int int2, String errorMsg) {
-		if(int1 != int2) {
-			System.out.println(errorMsg);
-			this.errors++;
-		}
-	}
-	
+	}	
 	
 	private void testArea() {
 		Arena arena = new Arena();	
@@ -52,6 +54,12 @@ public class TestStack
 		assertInt(arena.getArea(-0.5,-0.5), 8, "Error in testAre of fourth element");
 		assertInt(arena.getArea(1.5,1.5), -1, "Error in testAre of border element");		
 	
+	}	
+	
+	private void testDestruction() {
+		Company company1 = new Company("Facebook");
+		company1 = null;
+		System.gc();
 	}
 
 }
